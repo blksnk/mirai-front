cba33c35f153e361d504286e6c484fe06a326cf0
JN Veigel <art.jnveigel@gmail.com>
2019-07-20 16:24:39 +0200

    :sparkles: display page title dynamically :children_crossing: triggers refresh of document.title

---
+++ src/uikit/PageTitle.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { pageTitleSet } from 'store/actions/actions'
+
+import g from './uikit.module.css'
+
+const mapStateToProps = ({ pageTitle }) => ({ storeTitle: pageTitle })
+
+const PageTitle = ({ className, history, storeTitle, dispatch }) => {
+
+  history.listen((location) => {
+    const t = location.pathname.split('/')[1]
+    console.log('title: ', t)
+    if(storeTitle !== t) {
+      if(t === '') {
+        dispatch(pageTitleSet('home'))
+      } else {
+        dispatch(pageTitleSet(t))
+      }
+    }
+  })
+
+
+
+  return <span className={`${g.pageTitle} ${className}`}>{storeTitle}</span>
+}
+
+export default connect(mapStateToProps)(withRouter(PageTitle))
