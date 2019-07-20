480d763edce2e8219952b8d5ee29b0452f686577
JN Veigel <art.jnveigel@gmail.com>
2019-07-20 16:21:00 +0200

    :sparkles: img container with parallax effect, supports landscape and portrait props

---
+++ src/uikit/Img.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import s from './Img.module.css'
+import g from './uikit.module.css'
+
+const Img = ({ className, src, alt, landscape, ...props }) => {
+  return (
+      <div className={`${g.pWrapper} ${className} ${landscape ? s.landscape : s.portrait}`}>
+        <div className={g.frame1}>
+          <img src={src} alt={alt}/> 
+        </div>
+        <div className={g.frame2}>
+          <img src={src} alt=''/>   
+        </div>
+        <div className={g.frame3}></div>
+      </div>
+  )
+}
+
+export default Img
