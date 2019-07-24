5a2d12aaa3ed8b1d9a08e3aa984a8c3cae22877f
JN Veigel <art.jnveigel@gmail.com>
2019-07-24 20:33:01 +0200

    :sparkles: add email address display and svg logo

--- src/uikit/LeftNav.js
+++ src/uikit/LeftNav.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 
-import LogoText from 'uikit/LogoText'
+import Logo from 'uikit/Logo'
 import PageTitle from 'uikit/PageTitle'
 import PageIndicator from 'uikit/PageIndicator'
 
 import s from './LeftNav.module.css'
 
-const LeftNav = () => {
+const LeftNav = ({ history }) => {
   return (
   <nav className={s.nav}>
-    <LogoText/>
+    <Logo vertical history={history}/>
     <span className={s.branding}>freelance web development</span>
+    <ContactRedirect history={history}/>
 
     <div className={s.bottomWrapper}>
       <PageTitle className={s.pageTitle}/>
@@ -20,6 +22,12 @@ const LeftNav = () => {
   )
 }
 
+const ContactRedirect = ({ history }) => (
+  <a href='/contact' className={s.email} onClick={e => {
+    e.preventDefault()
+    if(window.location.pathname !== '/contact') {
+      history.push('/contact')
+  }}}>hello.mirai.dev@gmail.com</a>
+)
 
-
-export default LeftNav
+export default withRouter(LeftNav)
