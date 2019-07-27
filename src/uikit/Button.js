3c75c4a8b71e773c60a11d20fc5bad87e6280091
JN Veigel <art.jnveigel@gmail.com>
2019-07-27 02:19:29 +0200

    :recycle: simplify component :sparkles: dark mode enabled

--- src/uikit/Button.js
+++ src/uikit/Button.js
@@ -4,14 +4,10 @@ import g from './uikit.module.css'
 
 const Button = ({ children, className, submit, formId, disabled, title, onClick, ...props }) => {
   return (
-    <div className={`${className ? className : ''} ${g.btn} ${g.pWrapper} ${disabled ? g.disabled : ''}`}>
-      {submit
-        ? <input disabled={disabled} type='submit' value={children} title={title || ''} className={g.frame1}>
-          </input>
-        : <button title={title || ''} type='submit' className={g.frame1} onClick={() => {if(!disabled) onClick()}}>
-            <span>{children}</span>
-          </button>
-        }
+    <div className={`${className ? className : ''} btn ${g.btn} ${g.pWrapper} ${disabled ? g.disabled : ''}`}>
+      <button title={title || ''} type='submit' className={g.frame1} onClick={() => {if(!disabled) onClick()}}>
+        <span>{children}</span>
+      </button>
       <div className={g.frame2}></div>
     </div>
     )
