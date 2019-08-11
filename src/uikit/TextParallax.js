e2039811429c480d93c5ad76006edf8d8928d457
JN Veigel <art.jnveigel@gmail.com>
2019-08-11 02:03:49 +0200

    :sparkles: optional onClick prop for parallax text

--- src/uikit/TextParallax.js
+++ src/uikit/TextParallax.js
@@ -2,16 +2,21 @@ import React from 'react'
 
 import g from 'uikit/uikit.module.css'
 
-const TextParallax = ({ children }) => {
+const TextParallax = ({ children, onClick }) => {
   return (
     <div className={`${g.pWrapper} ${g.textPWrapper} textParallax`}>
-      <Fill className={`${g.frame1} fill ${g.textP1}`}>{children}</Fill>
+      <Fill onClick={onClick} className={`${g.frame1} fill ${g.textP1}`}>{children}</Fill>
       <Stroke i={2}>{children}</Stroke>
     </div>
   )
 }
 
-const Fill = ({ children, className }) => <h1 className={`${g.fill} ${className ? className : ''}`}>{children}</h1>
+const Fill = ({ children, className, onClick }) =>
+<h1
+  onClick={onClick ? onClick : null}
+  style={onClick ? { cursor: 'pointer' } : { } }
+  className={`${g.fill} ${className ? className : ''}`}
+>{children}</h1>
 
 const Stroke = ({ children, className, i })=> <h1 className={`${className} ${g[`frame${i}`]}`}>{children}</h1>
 
