c21b3d9add80d7d58225ff5977d38478d6f17587
JN Veigel <art.jnveigel@gmail.com>
2019-08-11 02:02:23 +0200

    :sparkles: implement drag-scrolling :lipstick: rm dynamic pointer cursor in favor of static move cursor

--- src/uikit/Slider.js
+++ src/uikit/Slider.js
@@ -4,6 +4,15 @@ import { formatNumber } from 'helpers/formatNumber'
 import g from 'uikit/uikit.module.css'
 
 export const InfoSlider = ({ array, className, numbers }) => {
+  const [ startX, setStartX ] = React.useState(0)
+  const [ scrollLeft, setScrollLeft ] = React.useState(0)
+
+  React.useEffect(() => {
+    addEvents(getSlider(className), startX, setStartX, scrollLeft, setScrollLeft)
+
+    return () => removeEvents(getSlider(className), startX, setStartX, scrollLeft, setScrollLeft)
+  }, [ className, startX, setStartX, scrollLeft, setScrollLeft ])
+
   return (
     <div className={`${className ? className : ''} ${g.slider}`}>
       {array.map((slide, i) => 
@@ -18,6 +27,53 @@ export const InfoSlider = ({ array, className, numbers }) => {
   )
 }
 
+const getSlider = (className) => {
+  let slider = document.querySelectorAll(`.${g.slider}`)[0]
+  if(className) {
+    slider = document.querySelector(`.${className}`)
+  }
+  return slider
+}
+let isDown = false
+
+const addEvents = (slider, startX, setStartX, scrollLeft, setScrollLeft) => {
+  slider.addEventListener('mousedown', e => allowDrag(e, slider, setStartX, setScrollLeft))
+  slider.addEventListener('mouseleave', e => eventLeave(slider))
+  slider.addEventListener('mouseup', e => eventLeave(slider))
+  slider.addEventListener('mousemove', e => scrollOnDrag(e, slider, startX, scrollLeft))
+}
+
+const removeEvents = (slider, startX, setStartX, scrollLeft, setScrollLeft) => {
+  slider.removeEventListener('mousedown', e => allowDrag(e, slider, setStartX, setScrollLeft))
+  slider.removeEventListener('mouseleave', e => eventLeave(slider))
+  slider.removeEventListener('mouseup', e => eventLeave(slider))
+  slider.removeEventListener('mousemove', e => scrollOnDrag(e, slider, startX, scrollLeft))
+}
+
+const eventLeave = (slider) => {
+  isDown = false
+}
+
+const allowDrag = (e, slider, setStartX, setScrollLeft) => {
+  if(!isDown) {
+    isDown = true
+    setStartX(e.pageX - slider.offsetLeft)
+    setScrollLeft(slider.scrollLeft)
+  }
+}
+
+const scrollOnDrag = (e, slider, startX, scrollLeft) => {
+  if(isDown) {
+    e.preventDefault()
+    const x = e.pageX - slider.offsetLeft
+    const walk = (x - startX) * 2.5 //scroll-fast
+    slider.scrollLeft = scrollLeft - walk
+    return false
+  } else {
+    return false
+  }
+}
+
 const RenderSlide = ({ slide, numbers, i }) => {
   const { img, alt, onClick, title, p, color } = slide
   return (
@@ -30,7 +86,6 @@ const RenderSlide = ({ slide, numbers, i }) => {
 
 const RenderFrame1 = ({ img, alt, onClick, numbers, i }) => (
   <div
-    style={onClick ? {cursor: 'pointer'} : {}}
     className={`${g.frame1} ${g.sliderImgContainer}`}
     onClick={onClick ? onClick : () => console.log('no onclick')}
   >
