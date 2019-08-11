0b88c8b98c902de9543c5a7641c465f5beee6280
JN Veigel <art.jnveigel@gmail.com>
2019-08-11 02:18:30 +0200

    :construction: WIP footer with contact information for contact view

---
+++ src/uikit/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import g from 'uikit/uikit.module.css'
+
+const Footer = () => 
+<React.Fragment>
+  
+</React.Fragment>
+
+export default Footer
